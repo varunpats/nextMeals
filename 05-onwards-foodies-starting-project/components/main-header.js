c154b26e4ac72c09c497af277bc1b6e49227e7ba
varunpats <varunpatil2502@gmail.com>
2026-02-12 22:18:30 +0530

    designed navigation header

---
+++ 05-onwards-foodies-starting-project/components/main-header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+
+export default function MainHeader() {
+    return <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+            <Image src={logo} alt="A plate with a food in it" />
+            NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+            <ul>
+                <li>
+                    <Link href="/meals">Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href="/community">Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
