49af93602f299ec2b5c639ca15b8e92179d07bb3
varunpats <varunpatil2502@gmail.com>
2026-02-19 23:32:00 +0530

    fetched data and added loading effect

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,6 +1,14 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+export async function Meals() {
+    const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
     return (<>
@@ -18,7 +26,9 @@ export default function MealsPage() {
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]}/>
+            <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                <Meals />
+            </Suspense>
         </main>
     </>
     )
