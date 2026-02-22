b76854272f2f5734c2432cd1be13f264207b6a3a
varunpats <varunpatil2502@gmail.com>
2026-02-22 22:57:18 +0530

    added error.js file

---
+++ 05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function error() {
+    return (
+        <main className="error">
+            <h1>An error occured</h1>
+            <p>Failed to fetch failed meals data. Please try again later.</p>
+        </main>
+    )
+}
