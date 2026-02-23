8026e4bfe044a33fe5522616eccdf312633d8b6c
varunpats <varunpatil2502@gmail.com>
2026-02-23 23:18:41 +0530

    developed individual meal page

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -6,3 +6,7 @@ export async function getMeals() {
     await new Promise((res) => setTimeout(res, 2000));
     return db.prepare('SELECT * FROM meals').all();
 }
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
