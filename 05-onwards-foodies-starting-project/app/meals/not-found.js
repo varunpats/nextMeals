21bb3144cbf02cfc50c143242fc3c7f1972e8557
varunpats <varunpatil2502@gmail.com>
2026-02-23 23:17:58 +0530

    designed not-found pages

---
+++ 05-onwards-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function notFound() {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>Unfourunately, we could not find requested meal data.</p>
+        </main>
+    )
+}
---
+++ 05-onwards-foodies-starting-project/app/not-found.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function notFound() {
+    return (
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Unfourunately, we could not found requested resource.</p>
+        </main>
+    )
+}
