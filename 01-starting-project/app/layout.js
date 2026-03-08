8e183a4e23872e06898630ee5064b4e060bf5570
varunpats <varunpatil2502@gmail.com>
2026-03-08 21:04:05 +0530

    designed layout and pages

--- 01-starting-project/app/layout.js
+++ 01-starting-project/app/layout.js
@@ -1,3 +1,4 @@
+import MainHeader from '@/components/main-header';
 import './globals.css';
 
 export const metadata = {
@@ -6,9 +7,14 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+        <MainHeader />
+        {children}
+        </div>
+      </body>
     </html>
   )
 }
