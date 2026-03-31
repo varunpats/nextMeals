d939e95514703d88fa87ef02beb81486d16bb2ef
varunpats <varunpatil2502@gmail.com>
2026-03-31 20:55:05 +0530

    added route groups and destructure file structure

--- 01-starting-project/app/(content)/news/[slug]/page.js
+++ 01-starting-project/app/(content)/news/[slug]/page.js
@@ -1,17 +1,20 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
     const newsSlug = params.slug;
     const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug)
 
-    if(!newsItem)
+    if (!newsItem)
         notFound();
 
     return (
         <article className="news-article">
             <header>
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                <Link href={`/news/${newsItem.slug}/image`}>
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                </Link>
                 <h1>{newsItem.title}</h1>
                 <time date={newsItem.date}>{newsItem.date}</time>
             </header>
