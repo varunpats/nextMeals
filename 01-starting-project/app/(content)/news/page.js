3cfedefff7a2d1c12caa67f35a847a3e19fa5cf2
varunpats <varunpatil2502@gmail.com>
2026-04-01 21:57:04 +0530

    changed news fetching

--- 01-starting-project/app/(content)/news/page.js
+++ 01-starting-project/app/(content)/news/page.js
@@ -1,11 +1,13 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+import { getAllNews } from "@/components/lib/news";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+    const news = getAllNews();
+
     return (
         <>
             <h1>News Page</h1>
-            <NewsList news={DUMMY_NEWS} />
+            <NewsList news={news} />
         </>
     )
 }
